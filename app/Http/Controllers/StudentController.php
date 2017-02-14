793faa506bf15aee39bf2029365c8a6c822f8aad
linzr <noctislin@gmail.com>
2017-02-14 13:34:41 +0800

    [C] Separate detail controller from index controller.

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use View;
+use App\Student;
+use App\Score;
 
 error_reporting(-1); // reports all errors
 ini_set("display_errors", "1"); // shows all errors
@@ -41,35 +43,5 @@ class StudentController extends Controller
     public function index() { 
         return view('index_empty'); 
     } 
-
-    public function detail($id) {
-        if (!is_numeric($id)){
-            return view('errors/404');
-        }
-        $students = DB::table('student')->get()->map(function ($value, $key) {
-            $arr = array('mc','tc','hw','bs','ks','ac');
-            foreach ($arr as $column) {
-                $value->{$column.'_i'} = explode(",", $value->{$column});
-                $value->{$column} = array_sum($value->{$column.'_i'});
-            }
-            $value->spe = $value->mc+$value->tc;
-            $value->dil = $value->hw+$value->bs+$value->ks+$value->ac;
-            $value->sum = $value->spe + $value->dil;
-            return $value;
-        })->sortByDesc(function ($a, $key) {
-            return $a->sum;
-        });
-
-        $leader = $students->first();
-        $student = $students->filter(function ($value, $key) use ($id) {
-            return $value->id == $id;
-        })->first();
-        if (! $student){
-            return view('errors/404');
-        } else {
-            return view('detail',['student' => $student, 'leader' => $leader]);
-        }
-    }
-
          
 }
