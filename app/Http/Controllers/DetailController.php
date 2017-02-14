ceea942d45006d5e4855ef1da09186e8c6dfacb8
linzr <noctislin@gmail.com>
2017-02-14 21:12:52 +0800

    [C] Update detail controller. Fix variable conflict.

--- app/Http/Controllers/DetailController.php
+++ app/Http/Controllers/DetailController.php
@@ -28,14 +28,14 @@ class DetailController extends Controller {
 
         $students = Student::all();
         $col = array('mc','tc','hw','bs','ks','ac');
-        foreach($students as $student){
-            $score = Score::find($student->latest_score_id);
+        foreach($students as $s){
+            $score = Score::find($s->latest_score_id);
             foreach ($col as $c) {
-                $student->{$c.'_i'} = explode(",", $score->{$c});
-                $student->{$c} = array_sum($student->{$c.'_i'});
-                $student->spe = $student->mc+$student->tc;
-                $student->dil = $student->hw+$student->bs+$student->ks+$student->ac;
-                $student->sum = $student->spe + $student->dil;
+                $s->{$c.'_i'} = explode(",", $score->{$c});
+                $s->{$c} = array_sum($s->{$c.'_i'});
+                $s->spe = $s->mc+$s->tc;
+                $s->dil = $s->hw+$s->bs+$s->ks+$s->ac;
+                $s->sum = $s->spe + $s->dil;
             }
         }
         $students = $students->sortByDesc(function ($a, $key) {
