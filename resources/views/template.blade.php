af9f8bd84aa2cbf4dbaeb355d06cb33bb4ac7079
linzr <noctislin@gmail.com>
2017-02-16 09:26:37 +0800

    [V]

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -28,12 +28,11 @@
    
   @include('_footer')
 
-  @yield('footer')
-
   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
   <script>
   $('div.alert').not('.alert-important').delay(3000).fadeOut(350);
   </script>
+  @yield('footer')
 </body>
 </html>
