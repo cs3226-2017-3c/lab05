d21613bf0b3256851f01d04c1a4e475b9ff59bae
linzr <noctislin@gmail.com>
2017-02-16 09:24:02 +0800

    [V] Update Js CDN.

--- resources/views/_footer.blade.php
+++ resources/views/_footer.blade.php
@@ -3,9 +3,3 @@
     <p class="text-right">© 2017 M. Ashmawi, C. G. Lim, Z. Lin, K. Xiao, S. F. Zarrinkoub & H. Zhao. <a href="https://github.com/cs3226-2017-3c" target="_blank"><img width="12" alt="GitHub" src="/img/GitHub-Mark-32px.png"></a></p>
       </div>
   </footer>
-
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-  <script>
-  $('div.alert').not('.alert-important').delay(3000).fadeOut(350);
-  </script>
--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -9,7 +9,7 @@
   <title>@yield('title') - CS3226 RankList 2020</title>
 
   <!-- Bootstrap -->    
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
   @yield('header')
   
 
@@ -29,5 +29,11 @@
   @include('_footer')
 
   @yield('footer')
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <script>
+  $('div.alert').not('.alert-important').delay(3000).fadeOut(350);
+  </script>
 </body>
 </html>
