0e2caaf2acb1461cb101b61bdf7627611b62a4ed
Sahand Zarrinkoub <sahandz@kth.se>
2017-02-21 22:00:46 +0800

    Implemented new function
    Function that sets the height of each row according to the selected
    attribute to sort by.

--- public/js/highlight.js
+++ public/js/highlight.js
@@ -1,7 +1,7 @@
 $(document).ready(function() 
 { 
     $("#myTable").tablesorter(); 
-    setAllDifferentHeight();
+    setHeightByAttr('Sum');
 } 
 ); 
 
@@ -14,14 +14,25 @@ function setAllSameHeight()
     
 }
 
-function setAllDifferentHeight()
+function setHeightByAttr(attr)
 {
+    //find the index of the attribute and save it in a variable called index
+
+    
+
     setTimeout(function() {
+        var index = null;
         var t = document.getElementById("myTable");
+        for(var i = 0; i < t.rows[0].cells.length; i++){
+            if(t.rows[0].cells[i].innerText == attr+"\n"){
+                index = i;
+                break
+            }
+        }
         t.rows[1].style.height = "30px";
         for(var i=2; i < t.rows.length; i++) {
-            var sumi = t.rows[i].cells[11].innerHTML;
-            var sumiminus1 = t.rows[i-1].cells[11].innerHTML;
+            var sumi = t.rows[i].cells[index].innerHTML;
+            var sumiminus1 = t.rows[i-1].cells[index].innerHTML;
             t.rows[i].style.height = 30+30*(Math.abs(sumi-sumiminus1)).toString()+"px";
 
         }   
