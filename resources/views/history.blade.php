c0960019096c94fea04f7f77b373665b1cebb8c4
XiaoKangwei <cocolonel.x@gmail.com>
2017-02-26 00:35:27 +0800

    - draw chart for sm, md

--- resources/views/history.blade.php
+++ resources/views/history.blade.php
@@ -7,10 +7,12 @@ Student History
 @endsection
 <div class="row">
 	<div class="hidden-xs hidden-sm hidden-md col-lg-12" >
-		<canvas id="historyChart" width="1000" height="500"></canvas>
-    <div id="js-legend" class="chart-legend"></div>
+		<canvas id="historyChart-lg" width="1000" height="500"></canvas>
 	</div>
-	<div class="hidden-lg col-md-12">
+  <div class="hidden-xs col-sm-12 col-md-12 hidden-lg" >
+    <canvas id="historyChart-sm" width="1000" height="500"></canvas>
+  </div>
+	<div class="col-xs-12 hidden-sm hidden-md hidden-lg">
 		<p>View in bigger screen for the history statistics=P</p>
 	</div>
 </div>
@@ -19,6 +21,77 @@ Student History
 	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>
 	<!-- draw history chart -->
 	<script>
+    $(function(){
+      var i, labels = [], week = "Week ";
+      for (i=0; i < 15; i=i+2) {
+        var aLabel = week.concat(i);
+        labels.push(aLabel);
+      }
+
+      var CSS_COLOR_NAMES = ["AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige","Bisque","Black","BlanchedAlmond","Blue","BlueViolet","Brown","BurlyWood","CadetBlue","Chartreuse","Chocolate","Coral","CornflowerBlue","Cornsilk","Crimson","Cyan","DarkBlue","DarkCyan","DarkGoldenRod","DarkGray","DarkGrey","DarkGreen","DarkKhaki","DarkMagenta","DarkOliveGreen","Darkorange","DarkOrchid","DarkRed","DarkSalmon","DarkSeaGreen","DarkSlateBlue","DarkSlateGray","DarkSlateGrey","DarkTurquoise","DarkViolet","DeepPink","DeepSkyBlue","DimGray","DimGrey","DodgerBlue","FireBrick","FloralWhite","ForestGreen","Fuchsia","Gainsboro","GhostWhite","Gold","GoldenRod","Gray","Grey","Green","GreenYellow","HoneyDew","HotPink","IndianRed","Indigo","Ivory","Khaki","Lavender","LavenderBlush","LawnGreen","LemonChiffon","LightBlue","LightCoral","LightCyan","LightGoldenRodYellow","LightGray","LightGrey","LightGreen","LightPink","LightSalmon","LightSeaGreen","LightSkyBlue","LightSlateGray","LightSlateGrey","LightSteelBlue","LightYellow","Lime","LimeGreen","Linen","Magenta","Maroon","MediumAquaMarine","MediumBlue","MediumOrchid","MediumPurple","MediumSeaGreen","MediumSlateBlue","MediumSpringGreen","MediumTurquoise","MediumVioletRed","MidnightBlue","MintCream","MistyRose","Moccasin","NavajoWhite","Navy","OldLace","Olive","OliveDrab","Orange","OrangeRed","Orchid","PaleGoldenRod","PaleGreen","PaleTurquoise","PaleVioletRed","PapayaWhip","PeachPuff","Peru","Pink","Plum","PowderBlue","Purple","Red","RosyBrown","RoyalBlue","SaddleBrown","Salmon","SandyBrown","SeaGreen","SeaShell","Sienna","Silver","SkyBlue","SlateBlue","SlateGray","SlateGrey","Snow","SpringGreen","SteelBlue","Tan","Teal","Thistle","Tomato","Turquoise","Violet","Wheat","White","WhiteSmoke","Yellow","YellowGreen"];
+      var arrayDataSets =[], dataSet = {}, data = [];
+      var currName = "", currWeek = 0, currStudent = 0;
+      @foreach($score as $sc)
+        var studentName = "{{$sc->student_name}}";
+        if (currName != studentName) {
+          if (data.length > 0) {
+            dataSet = {label: currName, fill: false, lineTension: 0, 
+              backgroundColor: CSS_COLOR_NAMES[currStudent],
+              borderColor: CSS_COLOR_NAMES[currStudent],
+              pointRadius: 0, data: data};
+            arrayDataSets.push(dataSet);
+            data = [];
+            currWeek = 0;
+            currStudent = currStudent + 1;
+            if (currStudent == CSS_COLOR_NAMES.length) {
+              currStudent = 0;
+            }
+          }
+          currName = studentName;
+        }
+        var thisDate = parseDate("{{$sc->effective_from}}");
+        while (convertWeekToDate(currWeek).compareTo(thisDate) < 0 ) {
+          if (data.length == 0) {
+            data.push(0);
+          } else {
+            var temp = data[currWeek-1];
+            data.push(temp);
+          }
+          currWeek = currWeek + 2;
+        }
+        if (data.length < currWeek) {
+          data.push("{{$sc->sum}}");
+        } else {
+          data[data.length-1] = "{{$sc->sum}}";
+        }
+      @endforeach
+
+      var historyData = {
+        labels : labels,
+        datasets : arrayDataSets
+      };
+      var history = $("#historyChart-sm");
+      var chartOptions = {
+        title: {
+          display: true,
+          fontSize: 20,
+          text: "Students\' Sum of Score over Week"
+        },
+        legend: {
+            display: true,
+            
+        }
+      };
+
+      var chartInstance = new Chart(history, {
+        type: 'line',
+        data: historyData,
+        options: chartOptions
+      });
+    });
+
+
+
     $(function(){
       var i, labels = [], week = "Week ";
       for (i=0; i < 15; i++) {
@@ -68,7 +141,7 @@ Student History
         labels : labels,
         datasets : arrayDataSets
       };
-      var history = $("#historyChart");
+      var history = $("#historyChart-lg");
       var chartOptions = {
         title: {
           display: true,
