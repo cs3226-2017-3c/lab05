bd15bf7a9c5c657f2d30bf2e406b735b070f2263
HangZhao <davidzhao800@gmail.com>
2017-02-25 14:15:21 +0800

    [V]add 500 error page

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,14 @@
+@extends('template')
+@section('title')
+Internal Server Error
+@endsection
+@section('main')
+<!-- Main component for a primary marketing message or call to action -->
+<div class="container-fluid">
+<div class="row" style="text-align:center">
+    <h1>500 - Internal Server Error</h1>
+    <p>Hey Bro, something went wrong, please contact our members. You may go to our <a href="/">home page</a>.</p>
+</div>
+</div>
+<!-- /container -->
+@endsection
