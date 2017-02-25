f4102e20661fc7c8f67b35ae143febee35732f87
chengim <chengim.sg@gmail.com>
2017-02-26 01:09:57 +0800

    achievement test

--- resources/views/achievementDetail.blade.php
+++ resources/views/achievementDetail.blade.php
@@ -25,7 +25,7 @@ Achievement Detail
 			@endif
 			
 			@foreach($students as $s)
-				@if($s->ac_i[0]!='x' && $s->ac_i[0]!='0')
+				@if($s->ac_i[7]!='x' && $s->ac_i[7]!='0')
 				{{$s->name}}
 				@endif
 			@endforeach
