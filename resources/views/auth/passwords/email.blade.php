f119b1cc6fa5599fcda698b1b70b8b6b927307fe
linzr <noctislin@gmail.com>
2017-02-26 18:25:21 +0800

    [V] Reset password template

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,6 +1,8 @@
-@extends('layouts.app')
-
-@section('content')
+@extends('template')
+@section('title')
+Reset Password
+@endsection
+@section('main')
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
