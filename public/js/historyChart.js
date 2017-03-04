086ed62ff8371fc1bb21096525af0647dab4376b
XiaoKangwei <cocolonel.x@gmail.com>
2017-03-04 22:43:39 +0800

    - Add explanation: legend is in descending order - Draw chart for small screen

--- public/js/historyChart.js
+++ public/js/historyChart.js
@@ -1,19 +1,83 @@
+var cacheDataSets;
+
 function drawChart(chartId){
+  var url = "historyDataSet";
+  var history = $(chartId);
   var i, labels = [], week = "Week ";
   for (i=0; i < 15; i++) {
     var aLabel = week.concat(i);
     labels.push(aLabel);
   }
-  var url = "historyDataSet";
   $.getJSON(url, function(dataSets){
-    var history = $(chartId);
+    ajaxReceived = true;
+    cacheDataSets = dataSets;
     var historyData = {
-        labels : labels,
-        datasets : dataSets
+      labels : labels,
+      datasets : dataSets
     };
     var chartInstance = new Chart(history, {
-        type: 'line',
-        data: historyData
+      type: 'line',
+      data: historyData
     });
   });
 }
+
+function drawChart(chartId, callback, anotherId){
+  var url = "historyDataSet";
+  var history = $(chartId);
+  var i, labels = [], week = "Week ";
+  for (i=0; i < 15; i++) {
+    var aLabel = week.concat(i);
+    labels.push(aLabel);
+  }
+  $.getJSON(url, function(dataSets){
+    ajaxReceived = true;
+    cacheDataSets = dataSets;
+    var historyData = {
+      labels : labels,
+      datasets : dataSets
+    };
+    var chartInstance = new Chart(history, {
+      type: 'line',
+      data: historyData
+    });
+    if (callback) {
+      callback(anotherId);
+    }
+  });
+}
+
+function drawChartSmall(chartId){
+  var history = $(chartId);
+  var i, labels = [], week = "Week ";
+  for (i=0; i < 15; i=i+2) {
+    var aLabel = week.concat(i);
+    labels.push(aLabel);
+  }
+  var dataSets = eliminateOddWeek(cacheDataSets);
+  var historyData = {
+    labels : labels,
+    datasets : dataSets
+  };
+  var chartInstance = new Chart(history, {
+    type: 'line',
+    data: historyData
+  });
+}
+
+function eliminateOddWeek(dataSets) {
+  var dataSetsCopy = dataSets;
+  var i;
+  var buffer;
+  for (i=0; i<dataSetsCopy.length; i++) {
+    var thisData = dataSetsCopy[i].data;
+    var tempData = new Array();
+    var j;
+    for (j=0; j<thisData.length; j=j+2) {
+      tempData.push(thisData[j]);
+    }
+    dataSetsCopy[i].data = tempData; 
+  }
+  return dataSetsCopy;
+}
+
--- resources/views/history.blade.php
+++ resources/views/history.blade.php
@@ -5,12 +5,14 @@ Student History
 @section('main')
 <div class="container">
 <div class="row">
-	<div class="hidden-xs hidden-sm hidden-md col-lg-12" >
+	<div class="hidden-xs hidden-sm hidden-md col-lg-12">
+		<p>The legend is sorted in <b>descending</b> order according to latest sum.</p>
 		<canvas id="historyChart-lg" width="800" height="500"></canvas>
 	</div>
-  <div class="hidden-xs col-sm-12 col-md-12 hidden-lg" >
-    <canvas id="historyChart-sm" width="800" height="500"></canvas>
-  </div>
+  	<div class="hidden-xs col-sm-12 col-md-12 hidden-lg">
+  		<p>The legend is sorted in <b>descending</b> order according to latest sum.</p>
+    	<canvas id="historyChart-sm" width="800" height="500"></canvas>
+  	</div>
 	<div class="col-xs-12 hidden-sm hidden-md hidden-lg">
 		<p>View in bigger screen for the history statistics=P</p>
 	</div>
@@ -21,5 +23,5 @@ Student History
 	<script type="text/javascript" src="../../js/historyChart.js"></script>
 	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>
 	<!-- draw history chart -->
-	<script>drawChart("#historyChart-lg")</script>
+	<script>drawChart("#historyChart-lg", drawChartSmall, "#historyChart-sm")</script>
 @endsection
