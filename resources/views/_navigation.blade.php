f8815b1b66e60ced6fcbff6c54da4744fa10d5ad
chengim <chengim.sg@gmail.com>
2017-03-05 20:15:17 +0800

    revert

--- resources/views/_navigation.blade.php
+++ resources/views/_navigation.blade.php
@@ -13,7 +13,7 @@
   		</div>
   		<div id="navbar" class="navbar-collapse collapse">
   			<ul class="nav navbar-nav">
-				<li id="home" @if(Request::path()=="/")class="active"@endif}}><a href="/">{{ route('home') }}</a></li>
+				<li id="home" @if(Request::path()=="/")class="active"@endif}}><a href="/">Home</a></li>
   				<li id="history" @if(Request::path()=="history")class="active"@endif}}><a href="/history">History</a></li>
 				<li id="achievement" @if(Request::path()=="achievement")class="active"@endif}}><a href="/achievement">Achievement</a></li>
           <li id="help" @if(Request::is('help'))class="active"@endif}}><a href="/help">Help</a></li>
